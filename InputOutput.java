ed6f4821618186403487856bbd612a348f986f75
NorisukiNZk <born.waffa@gmail.com>
2023-04-05 12:39:48 +0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class InputOutput{
  public static void main (String [] args){
   Scanner scaner = new Scanner(System.in);
+  String nama;
+  System.out.println("Masukan nama Anda :  ");
+  nama = scanner.nextLine();
+
  }
 }
